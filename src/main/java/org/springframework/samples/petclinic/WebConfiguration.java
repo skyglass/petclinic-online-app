c551ad839c01124d1cde8a300886c0c1b565ee8e
skyglass <skyglass2001@gmail.com>
2022-11-20 19:01:44 +0100

    try to fix resource loading behind load balancer

--- src/main/java/org/springframework/samples/petclinic/WebConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/WebConfiguration.java
@@ -7,9 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @Configuration
 public class WebConfiguration extends WebMvcConfigurationSupport {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/");
-    }
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+	}
+
 }
