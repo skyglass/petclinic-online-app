974db1a9960d44809e2cb58980178ac867e46ef4
skyglass <skyglass2001@gmail.com>
2022-11-20 19:22:20 +0100

    implemented jenkins ci pipeline

--- src/main/java/org/springframework/samples/petclinic/WebConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/WebConfiguration.java
@@ -9,7 +9,7 @@ public class WebConfiguration extends WebMvcConfigurationSupport {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+		registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/static/");
 	}
 
 }
